d12aafda28b02897472c68afecb1190a7a2eb3e3
Zuohuang Cai <zc26226@gmail.com>
2026-02-26 21:02:53 +0100

    update file structure

--- leads-api/bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Infrastructure\Lead\Providers\LeadServiceProvider::class,
-    App\Infrastructure\User\Providers\UserServiceProvider::class,
-];
