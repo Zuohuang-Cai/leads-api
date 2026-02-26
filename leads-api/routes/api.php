cac1b895f5af815fd30b76c8e7b14e05dec60b8b
Zuohuang Cai <zc26226@gmail.com>
2026-02-26 20:47:50 +0100

    delete useless methode

--- leads-api/routes/api.php
+++ leads-api/routes/api.php
@@ -9,7 +9,6 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function (): void {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
-    Route::get('/verify-email', [AuthController::class, 'verifyEmail']);
 });
 
 // Protected Routes
@@ -19,7 +18,6 @@ Route::middleware('auth:sanctum')->group(function (): void {
     Route::prefix('auth')->group(function (): void {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/me', [AuthController::class, 'me']);
-        Route::post('/send-verification-email', [AuthController::class, 'sendVerificationEmail']);
     });
 
     // Leads
