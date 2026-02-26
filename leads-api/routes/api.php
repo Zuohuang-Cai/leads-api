b959a1ea1b2335a7a88dfe6feacfacbfca887cc4
Zuohuang Cai <zc26226@gmail.com>
2026-02-26 13:43:08 +0100

    added routes for authentication and leads management

--- leads-api/routes/api.php
+++ leads-api/routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Api\Auth\Controllers\AuthController;
+use App\Http\Api\Leads\Controllers\LeadController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('auth')->group(function (): void {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth:sanctum')->group(function (): void {
+
+    Route::prefix('auth')->group(function (): void {
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me', [AuthController::class, 'me']);
+    });
+
+    Route::apiResource('leads', LeadController::class);
+});
